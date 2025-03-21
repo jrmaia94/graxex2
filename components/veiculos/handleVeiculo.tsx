2c224315e4a93ab12ce4290bdeed9badffd0c7c7
jrmaia94 <euilis.jr@gmail.com>
2025-03-21 11:00:50 -0300

    fix: resolved responsivity in mobile

--- components/veiculos/handleVeiculo.tsx
+++ components/veiculos/handleVeiculo.tsx
@@ -9,6 +9,7 @@ import {
 } from "../ui/dialog";
 import { useSession } from "next-auth/react";
 import { FormVeiculo } from "./formVeiculo";
+import { ScrollArea } from "../ui/scroll-area";
 
 interface HandleVeiculoProps {
   children: ReactNode;
@@ -25,7 +26,7 @@ const HandleVeiculo = ({ children }: HandleVeiculoProps) => {
       {data?.user && (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
           <DialogTrigger asChild>{children}</DialogTrigger>
-          <DialogContent className="max-w-[95%] rounded-md p-2 m-0">
+          <DialogContent className="max-w-[95%] h-[90%] rounded-md p-2 m-0">
             <DialogHeader className="text-left">
               <DialogTitle>Cadastro de veículos</DialogTitle>
               <DialogDescription>
@@ -33,7 +34,9 @@ const HandleVeiculo = ({ children }: HandleVeiculoProps) => {
                 carregar os dados do veículo
               </DialogDescription>
             </DialogHeader>
-            <FormVeiculo setIsOpen={setIsOpen} user={data.user} />
+            <ScrollArea className="h-full">
+              <FormVeiculo setIsOpen={setIsOpen} user={data.user} />
+            </ScrollArea>
           </DialogContent>
         </Dialog>
       )}
