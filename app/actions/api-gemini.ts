a0547e47350b55400698f82f5f97b0ee49796c93
jrmaia94 <euilis.jr@gmail.com>
2025-12-01 22:38:39 -0300

    gemini config

--- app/actions/api-gemini.ts
+++ app/actions/api-gemini.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
 
 // Access your API key (see "Set up your API key" above)
-const genAI: any =
-  process.env.GEMINI_API_KEY &&
-  new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const apiKey = process.env.GEMINI_API_KEY;
+if (!apiKey) throw new Error("GEMINI_API_KEY is not defined");
+const genAI = new GoogleGenerativeAI(apiKey);
 
 // Converts a File object to a GoogleGenerativeAI.Part object.
 async function fileToGenerativePart(file: File) {
@@ -21,13 +21,55 @@ async function fileToGenerativePart(file: File) {
 
 export async function sendImage(imageParts: any[]) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI?.getGenerativeModel({ model: "gemini-1.5-flash" });
+  if (genAI === undefined) throw new Error("GEMINI_API_KEY is not defined");
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   /*   const prompt = `A imagem a seguir deve ser de um veículo do tipo caminhão ou ônibus. Caso não consiga identificar essa imagem como um veículo você vai me responder com "{"code": 400}". Mas se conseguir identificar o veículo, preciso que me diga qual o modelo, a marca, a cor, a empresa e a placa desse veículo. Preciso que retorne essa informações em JSON de linha única onde todas as chaves são no formato string.`; */
-  const prompt = `Analisar a imagem anexada e identificar o veículo. Se for um caminhão ou ônibus, fornecer as seguintes informações em formato JSON escrito em linha única: modelo, fabricante, cor, empresa, numero_da_frota e placa. Se uma dessas informações não for encontrada dê o valor "". Caso não consiga identificar essa imagem como um veículo você vai me responder com "{"code": 400}"`;
+  const prompt = `Analisar a imagem anexada e identificar o veículo. Fornecer as informações de acordo com o responseSchema fornecido. Se uma das informações não for encontrada dê o valor "". Responder apenas com o JSON solicitado, sem texto adicional.`;
+
+  model.generationConfig = {
+    responseMimeType: "application/json",
+    responseSchema: {
+      type: SchemaType.OBJECT,
+      properties: {
+        modelo: {
+          type: SchemaType.STRING,
+          description: "Modelo do veículo",
+          example: "FH 540",
+        },
+        fabricante: {
+          type: SchemaType.STRING,
+          description: "Fabricante do veículo",
+          example: "Volvo",
+        },
+        cor: {
+          type: SchemaType.STRING,
+          description: "Cor do veículo",
+          example: "Vermelho",
+        },
+        empresa: {
+          type: SchemaType.STRING,
+          description: "Empresa proprietária do veículo",
+          example: "Transportes XYZ",
+        },
+        numero_da_frota: {
+          type: SchemaType.STRING,
+          description: "Número da frota do veículo",
+          example: "123",
+        },
+        placa: {
+          type: SchemaType.STRING,
+          description: "Placa do veículo",
+          example: "ABC-1234",
+        },
+      },
+    },
+  };
 
   const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
+
+  const response = result.response;
+  console.log(response);
   const json = response
     .text()
     .toString()
