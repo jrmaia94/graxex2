211e8b69aa4959fe7f49292f7a1a369f21370b0d
jrmaia94 <euilis.jr@gmail.com>
2026-03-16 19:01:58 -0300

    Revert "snowfall"
    This reverts commit 98c3cb0d2041288fc9bdc5cccd13b09de8204bbd.

--- components/snowfall.tsx
+++ components/snowfall.tsx
@@ -2,9 +2,5 @@
 import Snowfall from "react-snowfall";
 
 export const SnowfallComponent = () => {
-  return (
-    <div className="w-screen h-screen fixed">
-      <Snowfall enable3DRotation color="#bdd4da" />
-    </div>
-  );
+  return <Snowfall color="#82C3D9" />;
 };
